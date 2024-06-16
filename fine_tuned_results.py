a68bacb4e2f5c22ae13833a20d5283dc886ef2e0
blefo <lefort.baptiste@yahoo.fr>
2024-06-16 21:30:02 +0200

    - Added the result inference code

---
+++ fine_tuned_results.py
@@ -0,0 +1,69 @@
+import mistralai
+from mistralai.exceptions import MistralAPIStatusException, MistralException
+from mistralai.models.chat_completion import ChatMessage
+from mistralai.client import MistralClient
+
+from tqdm import tqdm
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import backoff
+import requests
+
+
+def prompt(row):
+    return f"""
+        You are provided with an image that has been converted to ASCII art and your task is to classify it among 10 categories.
+        Here are the possible categories: T-shirt/top, Trouser, Pullover, Dress, Coat, Sandal, Shirt, Sneaker, Bag, Ankle boot.
+
+        You MUST only output the identified class and nothing else.
+
+        ### ASCII art image:
+        {row['text']}
+
+        ### Response:
+    """
+
+
+@backoff.on_exception(backoff.expo,
+                      (requests.exceptions.RequestException,
+                                MistralAPIStatusException,
+                                MistralException))
+def get_inference(index, llm_input_value: str):
+    chat_response = client.chat(
+        model=client.jobs.retrieve("be2e7a1c-21e3-458a-881c-9d9adca22cef").fine_tuned_model,
+        messages=[ChatMessage(role='user', content=llm_input_value)]
+    )
+    return index, chat_response.choices[0].message.content
+
+
+if __name__ == "__main__":
+    api_key = "puueDsQhS9LXPdEnooegEai7NeT4oer4"  # This key has been revoked
+    client = MistralClient(api_key=api_key)
+
+    # Init the validation data
+    validation_data = pd.read_csv('transformed_data/test.csv', index_col=0)[150:]
+    validation_data = validation_data.reset_index(drop=True)
+    validation_data['llm_input'] = validation_data[['text']].apply(lambda x: prompt(x), axis=1)
+
+    results = []
+
+    with ThreadPoolExecutor(max_workers=40) as executor:
+        futures = [executor.submit(get_inference, i, validation_data['llm_input'].loc[i]) for i in
+                   range(validation_data.shape[0])]
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            results.append(future.result())
+
+    # Sort results by index
+    results.sort(key=lambda x: x[0])
+
+    # Extract only the response part
+    llm_results = pd.Series([result[1] for result in results], name="llm_response")
+
+    # Concatenate with the original validation data
+    validation_data = pd.concat([validation_data, llm_results], axis=1)
+
+    print('''------ Saving File --------''')
+
+    validation_data.to_csv('transformed_data/results_on_validation.csv')
+
+    print('''------ Experiment Done --------''')
--- fine_tuning.py
+++ fine_tuning.py
@@ -1,10 +1,13 @@
 import os
 from mistralai.client import MistralClient
 from mistralai.models.jobs import WandbIntegrationIn, TrainingParameters
+import time
 
 api_key = "puueDsQhS9LXPdEnooegEai7NeT4oer4"    # This key has been revoked
 wandb_api_key = "062fb4bbe403b946cfaa4f52792fe7d7133c0274"  # This key has been revoked
 client = MistralClient(api_key=api_key)
+
+model_name: str = "open-mixtral-8x7b" #"open-mistral-7b" # open-mixtral-8x7b
 
 if __name__ == "__main__":
     print('----- Starting file creation -----')
@@ -18,11 +21,11 @@ if __name__ == "__main__":
     print('----- Starting Fine Tuning Job -----')
 
     created_jobs = client.jobs.create(
-        model="open-mistral-7b",
+        model=model_name,
         training_files=[vision_mnist_train.id],
         validation_files=[vision_mnist_test.id],
         hyperparameters=TrainingParameters(
-            training_steps=30,
+            training_steps=100,
             learning_rate=0.0001,
         ),
         integrations=[
@@ -34,6 +37,12 @@ if __name__ == "__main__":
         ],
     )
 
-    print(created_jobs)
+    print(f"""----- Job initialzed {created_jobs} -----""")
 
-    print(f'''----- All Done -> Job is Running with id {created_jobs.id} -----''')
+    retrieved_job = client.jobs.retrieve(created_jobs.id)
+    while retrieved_job.status in ["RUNNING", "QUEUED"]:
+        retrieved_job = client.jobs.retrieve(created_jobs.id)
+        print(f""" ----- Found a running job {retrieved_job} ------- """)
+        time.sleep(30)
+
+    print(f'''----- All Done -> Job is Done with id {created_jobs.id} -----''')
