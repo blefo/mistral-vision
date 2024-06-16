f82abd251748b0b7e2f49834b5dd82612597243f
blefo <lefort.baptiste@yahoo.fr>
2024-06-16 13:47:07 +0200

    - Added multiprocessing (28 seconds for processing all the data instead of at least 15 minutes)

--- data_process.py
+++ data_process.py
@@ -7,6 +7,7 @@ import numpy as np
 from torchvision.transforms import functional as F
 from torchvision.transforms import v2
 from tqdm import tqdm
+import multiprocessing as mp
 
 
 # Function to convert an image to ASCII
@@ -115,6 +116,85 @@ def load_data(dataset_name: str, transform: bool = False):
 
     return train_dataset, valid_dataset, test_dataset
 
+def worker(dataset, start, end, result_queue):
+    dataset_text, dataset_target = [], []
+    for i in range(start, min(end, dataset.__len__())):
+        item_val = dataset.__getitem__(i, minimalist=True)
+        dataset_text.append(item_val[0])
+        dataset_target.append(item_val[1])
+    result_queue.put((dataset_text, dataset_target))
+
+def convert_and_save_to_dataframe_multiprocessing(train_dataset, valid_dataset, test_dataset):
+    train_dataset_len = train_dataset.__len__()
+    valid_dataset_len = valid_dataset.__len__()
+    test_dataset_len = test_dataset.__len__()
+
+    num_processes = mp.cpu_count()  # Get the number of available cores
+
+    # Create queues to store the results
+    train_queue = mp.Queue()
+    valid_queue = mp.Queue()
+    test_queue = mp.Queue()
+
+    # Create and start the processes
+    processes = []
+
+    # Train dataset processes
+    chunk_size = (train_dataset_len + num_processes - 1) // num_processes
+    for i in range(num_processes):
+        start = i * chunk_size
+        end = start + chunk_size
+        process = mp.Process(target=worker, args=(train_dataset, start, end, train_queue))
+        processes.append(process)
+        process.start()
+
+    # Valid dataset processes
+    chunk_size = (valid_dataset_len + num_processes - 1) // num_processes
+    for i in range(num_processes):
+        start = i * chunk_size
+        end = start + chunk_size
+        process = mp.Process(target=worker, args=(valid_dataset, start, end, valid_queue))
+        processes.append(process)
+        process.start()
+
+    # Test dataset processes
+    chunk_size = (test_dataset_len + num_processes - 1) // num_processes
+    for i in range(num_processes):
+        start = i * chunk_size
+        end = start + chunk_size
+        process = mp.Process(target=worker, args=(test_dataset, start, end, test_queue))
+        processes.append(process)
+        process.start()
+
+    # Collect the results from the queues
+    train_dataset_text, train_dataset_target = [], []
+    valid_dataset_text, valid_dataset_target = [], []
+    test_dataset_text, test_dataset_target = [], []
+
+    for _ in tqdm(range(num_processes)):
+        train_text, train_target = train_queue.get()
+        valid_text, valid_target = valid_queue.get()
+        test_text, test_target = test_queue.get()
+
+        train_dataset_text.extend(train_text)
+        train_dataset_target.extend(train_target)
+        valid_dataset_text.extend(valid_text)
+        valid_dataset_target.extend(valid_target)
+        test_dataset_text.extend(test_text)
+        test_dataset_target.extend(test_target)
+
+    for process in processes:
+        process.join()
+
+    train_dataset_dict = {'text': train_dataset_text, 'target': train_dataset_target}
+    valid_dataset_dict = {'text': valid_dataset_text, 'target': valid_dataset_target}
+    test_dataset_dict = {'text': test_dataset_text, 'target': test_dataset_target}
+
+    train_dataset_df = pd.DataFrame(train_dataset_dict)
+    valid_dataset_df = pd.DataFrame(valid_dataset_dict)
+    test_dataset_df = pd.DataFrame(test_dataset_dict)
+
+    return train_dataset_df, valid_dataset_df, test_dataset_df
 
 def convert_and_save_to_dataframe(train_dataset, valid_dataset, test_dataset):
     train_dataset_text, train_dataset_target = [], []
@@ -153,7 +233,7 @@ def convert_and_save_to_dataframe(train_dataset, valid_dataset, test_dataset):
 if __name__ == '__main__':
     train_dataset, valid_dataset, test_dataset = load_data("MNIST", True)
 
-    train_dataset, valid_dataset, test_dataset = convert_and_save_to_dataframe(train_dataset, valid_dataset, test_dataset)
+    train_dataset, valid_dataset, test_dataset = convert_and_save_to_dataframe_multiprocessing(train_dataset, valid_dataset, test_dataset)
 
     train_dataset.to_csv('transformed_data/train.csv')
     valid_dataset.to_csv('transformed_data/valid.csv')
